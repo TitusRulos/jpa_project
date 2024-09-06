3b9264323c5877814f5cc090c229479e7ddc05ff
TitusRulos <jujotiga@gmail.com>
2024-09-06 05:53:46 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   pom.xml
    	modified:   src/main/java/com/example/jpa_project/Roles/application/RolesService.java
    	modified:   src/main/java/com/example/jpa_project/Roles/domain/repository/RolesRepository.java
    	modified:   src/main/java/com/example/jpa_project/Roles/infrastructure/RolesController.java
    	modified:   src/main/java/com/example/jpa_project/SubresponseOptions/infrastructure/SubresponseOptionsController.java

--- src/main/java/com/example/jpa_project/Roles/application/RolesService.java
+++ src/main/java/com/example/jpa_project/Roles/application/RolesService.java
@@ -1,46 +1,46 @@
-package com.example.jpa_project.Roles.application;
+// package com.example.jpa_project.Roles.application;
 
-import com.example.jpa_project.Roles.domain.repository.RolesRepository;
-import com.example.jpa_project.Roles.domain.models.Roles;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
+// import com.example.jpa_project.Roles.domain.repository.RolesRepository;
+// import com.example.jpa_project.Roles.domain.models.Roles;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import java.util.List;
+// import java.util.Optional;
 
-@Service
-public class RolesService {
+// @Service
+// public class RolesService {
 
-    @Autowired
-    private RolesRepository repository;
+//     @Autowired
+//     private RolesRepository repository;
 
-    public List<Roles> findAll() {
-        return repository.findAll();
-    }
+//     public List<Roles> findAll() {
+//         return repository.findAll();
+//     }
 
-    public Optional<Roles> findById(Long id) {
-        return repository.findById(id);
-    }
+//     public Optional<Roles> findById(Long id) {
+//         return repository.findById(id);
+//     }
 
-    public Roles save(Roles question) {
-        return repository.save(question);
-    }
+//     public Roles save(Roles question) {
+//         return repository.save(question);
+//     }
 
-    public Optional<Roles> update(Long id, Roles responseOptions) {
-        Optional<Roles> chapterOpt = repository.findById(id);
+//     public Optional<Roles> update(Long id, Roles responseOptions) {
+//         Optional<Roles> chapterOpt = repository.findById(id);
 
-        if (chapterOpt.isPresent()) {
-            Roles existingResponseQuestions = chapterOpt.orElseThrow();
-            return Optional.of(repository.save(existingResponseQuestions));
-        }
+//         if (chapterOpt.isPresent()) {
+//             Roles existingResponseQuestions = chapterOpt.orElseThrow();
+//             return Optional.of(repository.save(existingResponseQuestions));
+//         }
 
-        return chapterOpt;
-    }
+//         return chapterOpt;
+//     }
 
-    public Optional<Roles> delete(Long id) {
-        Optional<Roles> chapterOpt = repository.findById(id);
-        chapterOpt.ifPresent(chapterItem -> {
-            repository.delete(chapterItem);
-        });
-        return chapterOpt;
-    }
-}
+//     public Optional<Roles> delete(Long id) {
+//         Optional<Roles> chapterOpt = repository.findById(id);
+//         chapterOpt.ifPresent(chapterItem -> {
+//             repository.delete(chapterItem);
+//         });
+//         return chapterOpt;
+//     }
+// }
--- src/main/java/com/example/jpa_project/SubresponseOptions/infrastructure/SubresponseOptionsController.java
+++ src/main/java/com/example/jpa_project/SubresponseOptions/infrastructure/SubresponseOptionsController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/response-questions")
+@RequestMapping("/subresponse-questions")
 public class SubresponseOptionsController {
     private final SubresponseOptionsService service;
 
