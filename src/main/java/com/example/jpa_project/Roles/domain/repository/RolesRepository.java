f2a8517d5c8ca70bd875b8e3ed3c2c43d0bf05ce
TitusRulos <jujotiga@gmail.com>
2024-09-06 06:14:24 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/main/java/com/example/jpa_project/Roles/application/RolesService.java
    	modified:   src/main/java/com/example/jpa_project/Roles/domain/repository/RolesRepository.java
    	modified:   src/main/java/com/example/jpa_project/Roles/infrastructure/RolesController.java

--- src/main/java/com/example/jpa_project/Roles/domain/repository/RolesRepository.java
+++ src/main/java/com/example/jpa_project/Roles/domain/repository/RolesRepository.java
@@ -1,21 +1,21 @@
-// package com.example.jpa_project.Roles.domain.repository;
+package com.example.jpa_project.Roles.domain.repository;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.jpa_project.Roles.domain.models.Roles;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.jpa_project.Roles.domain.models.Roles;
+import org.springframework.stereotype.Repository;
 
-// @Repository
-// public interface RolesRepository extends JpaRepository<Roles, Long> {
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Long> {
 
-//     List<Roles> findAll();
+    List<Roles> findAll();
 
-//     Optional<Roles> findById(Long id);
+    Optional<Roles> findById(Long id);
 
-//     // Roles save(Roles question);
+    // Roles save(Roles question);
 
-//     void delete(Roles chapterItem);
+    void delete(Roles chapterItem);
 
-// }
+}
